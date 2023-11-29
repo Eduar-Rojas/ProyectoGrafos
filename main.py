7ff906d58e0ccdb67dd61d5127c1f263d6265d00
Lalo <eduardo.rojas2020@alu.uct.cl>
2023-11-29 18:21:24 -0300

    Agrego comentarios

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 class Vertex:
+    # Constructor
     def __init__(self, name):
         self.name = name
-    def get_name(self):
-        return self.name
+    # Obtener datos de la clase
     def __str__(self):
+        return self.name
+    #Gets and sets
+    def get_name(self):
         return self.name
 
 class Edge:
+    #Constructor
     def __init__(self, vertex_1, vertex_2):
         self.vertex_1 = vertex_1
-        self.vertex_2 = vertex_2    
+        self.vertex_2 = vertex_2
+
+    # Obtener datos de la clase
+    def __str__(self):
+        return self.vertex_1.get_name() + " ---> " + self.vertex_2.get_name()
+
+    #Gets and sets
     def get_vertex_1(self):
         return self.vertex_1
     def get_vertex_2(self):
         return self.vertex_2
-    def __str__(self):
-        return self.vertex_1.get_name() + " ---> " + self.vertex_2.get_name()
