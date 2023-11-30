0406bd5e4aec78387ecbbfedffa36b5d4a679d1d
Lalo <eduardo.rojas2020@alu.uct.cl>
2023-11-30 20:24:40 -0300

    Arreglos en metodos e imlpementado algortimo prim
    BFS y DFS están listos.

--- main2.py
+++ main2.py
@@ -1,3 +1,4 @@
+import heapq 
 class Vertex:
     # Constructor
     def __init__(self, name):
@@ -13,9 +14,10 @@ class Vertex:
 
 class Edge:
     # Constructor
-    def __init__(self, vertex_1, vertex_2):
+    def __init__(self, vertex_1, vertex_2, weight):
         self.vertex_1 = vertex_1
         self.vertex_2 = vertex_2
+        self.weight = weight
 
     # Obtener datos de la clase
     def __str__(self):
@@ -28,35 +30,48 @@ class Edge:
     def get_vertex_2(self):
         return self.vertex_2
 
+    def get_weight(self):
+        return self.weight
+
 class Graph:
     # Constructor, crea un grafo con un diccionario
-    def __init__(self):
+    def __init__(self, directed=True):
         self.graph_dict = {}
+        self.directed = directed
 
+    # Obtener datos del grafo
     def __str__(self):
-        all_edges = ""
-        for vertex in self.graph_dict:
-            for neighbors in self.graph_dict[vertex]:
-                all_edges += vertex.get_name() + " ---> " + neighbors.get_name() + "\n"
-        return all_edges
+        graph_dict = ""
+        for vertex, neighbors in self.graph_dict.items():
+            for neighbor, weight in neighbors:
+                graph_dict += f"{vertex.get_name()} ---({weight})---> {neighbor.get_name()}\n"
+        return graph_dict
 
-    # Añade un vértice al grafo
+    # Añade un vértice al grafo (él vertice debe estár creado)
     def add_vertex(self, vertex):
         if vertex in self.graph_dict:
-            return "El vértice ya está en el grafo"
+            return print("El Vertice ya está en el grafo:",vertex)
         self.graph_dict[vertex] = []
+        print(f"el vertice: {vertex} se agregó al diccionario")
 
+    # Añade una arista al grafo (la arista debe estár creada)
     def add_edge(self, edge):
-        v1 = edge.get_vertex_1()
-        v2 = edge.get_vertex_2()
-        self.graph_dict[v1].append(v2)
-        self.graph_dict[v2].append(v1)  # Agregar esta línea si el grafo es no dirigido
+        vertex_1 = edge.get_vertex_1()
+        vertex_2 = edge.get_vertex_2()
+        weight = edge.get_weight()
+        self.graph_dict[vertex_1].append((vertex_2, weight))
+        if not self.directed:
+            self.graph_dict[vertex_2].append((vertex_1, weight))  # Agregar esta línea si el grafo es no dirigido
 
+    # verificar si un vertice existe
     def get_vertex(self, vertex_name):
         for v in self.graph_dict:
             if vertex_name == v.get_name():
-                return v
+                return print("existe ", v)
         print(f"El vétice {vertex_name} no existe")
+
+    #Topological sort
+    
 
     def BFS(self, start):
         visited = []
@@ -70,6 +85,7 @@ class Graph:
             print(s, end=" ")
 
             for neighbour in self.graph_dict[s]:
+                neighbour = neighbour[0]
                 if neighbour not in visited:
                     visited.append(neighbour)
                     queue.append(neighbour)
@@ -83,11 +99,62 @@ class Graph:
             visited.add(start)
 
             for neighbour in self.graph_dict[start]:
+                neighbour = neighbour[0]
                 if neighbour not in visited:
                     self.DFS(neighbour, visited)
 
-# Programa principal
-g = Graph()
+    def dijkstra(self, initial):
+        print("Algoritmo Dijkstra ejecutándose: ")
+        visited = {initial.get_name(): 0}
+        path = {}
+
+        nodes = set(self.graph_dict.keys())
+
+        while nodes:
+            min_node = None
+            for node in nodes:
+                if node in visited:
+                    if min_node is None or visited[node] < visited[min_node]:
+                        min_node = node
+
+            if min_node is None:
+                break
+
+            nodes.remove(min_node)
+            current_weight = visited[min_node]
+
+            for neighbor, weight in self.graph_dict[min_node]:
+                total_weight = current_weight + weight
+                if neighbor not in visited or total_weight < visited[neighbor]:
+                    visited[neighbor] = total_weight
+                    path[neighbor] = min_node
+
+        return visited, path
+
+    def prim(self):
+        visitados = set()
+        arbolSM = []
+        suma_total = 0
+        nodoI = list(self.graph_dict.keys())[0]
+        cola = [(0, nodoI)]
+
+        while cola:
+            peso, nodoA = heapq.heappop(cola)
+
+            if nodoA not in visitados:
+                visitados.add(nodoA)
+                arbolSM.append((nodoA.get_name(), peso))  # Cambiar aquí
+                suma_total += peso
+
+                if nodoA in self.graph_dict:
+                    for vecino, pesoB in self.graph_dict[nodoA]:
+                        if vecino not in visitados:
+                            heapq.heappush(cola, (pesoB, vecino))
+
+        return arbolSM, suma_total
+
+# -----------------------Programa principal--------------------------------
+g = Graph(directed=False)
 
 # Creando vértices
 vertex_a = Vertex('a')
@@ -95,16 +162,29 @@ vertex_b = Vertex('b')
 vertex_c = Vertex('c')
 vertex_d = Vertex('d')
 
-# Añadir vértices al grafo 
+# Añadir vértices al grafo                       
 g.add_vertex(vertex_a)
 g.add_vertex(vertex_b)
 g.add_vertex(vertex_c)
 g.add_vertex(vertex_d)
 
-# Creamos aristas a partir de los vértices existentes en el grafo
-g.add_edge(Edge(vertex_a, vertex_b))
-g.add_edge(Edge(vertex_a, vertex_c))
-g.add_edge(Edge(vertex_b, vertex_d))
+# Creamos aristas a partir de los vértices existentes en el grafo 
+g.add_edge(Edge(vertex_a, vertex_b, 1))
+g.add_edge(Edge(vertex_a, vertex_d, 5))
+g.add_edge(Edge(vertex_b, vertex_c, 4))
+g.add_edge(Edge(vertex_b, vertex_d, 5))
+g.add_edge(Edge(vertex_c, vertex_d, 2))
+g.add_edge(Edge(vertex_b, vertex_d, 5))
+
+# Si fuera no dirigido:
+# graph_dict = {'a': [('b', 1), ('c', 2)],
+#               'b': [('a', 1), ('d', 3)],
+#               'c': [('a', 2)]}
+
+# Si fuera dirigido:
+# graph_dict = {'a': [('b', 1), ('c', 2)],
+#               'b': [('d', 3)],
+#               'c': [()]}
 
 # Método del grafo BFS
 
@@ -112,8 +192,17 @@ g.add_edge(Edge(vertex_b, vertex_d))
 
 # Método del grafo DFS
 
-#g.DFS(vertex_a)
+#g.DFS(vertex_c)
 
 # Mostrar adyacencias de vértices
 
 print(g)
+
+arbol_expansion_minima, suma_total = g.prim()
+print("Árbol de expansión mínima:", arbol_expansion_minima)
+print("Suma total del árbol:", suma_total)
+
+print(g.dijkstra(vertex_a))
+
+# ({'A': 0, 'B': 1, 'C': 2, 'D': 4}, {'B': 'A', 'C': 'A', 'D': 'B'})
+# ({'a': 0}, {})
